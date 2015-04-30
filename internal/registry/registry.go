ec1aa563ad5e3eade7b808ea8c4c74298734a85b
Vlad Didenko <git@didenko.com>
2015-04-30 18:34:06 -0500

    Tested Registry implementation

---
+++ internal/registry/registry.go
@@ -0,0 +1,145 @@
+/*
+	(c) 2015 Vlad Didenko. All Rights Reserved.
+
+	This file is part of Port ALLocator tool a.k.a. pall.
+
+	pall is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	pall is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with pall.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+package registry
+
+import (
+	"fmt"
+	"sync"
+)
+
+type Registry struct {
+	sync.RWMutex
+	byname    map[string]*service
+	byport    map[uint16]*service
+	port_min  uint16
+	port_max  uint16
+	port_next uint16
+}
+
+func New(min, max uint16) (*Registry, error) {
+	if min > max {
+		return nil,
+			fmt.Errorf("Minimum port %d must be less than maximum port %d",
+				min, max)
+	}
+
+	return &Registry{
+		byname:    make(map[string]*service, 100),
+		byport:    make(map[uint16]*service, 100),
+		port_min:  min,
+		port_max:  max,
+		port_next: min,
+	}, nil
+}
+
+func (r *Registry) Query(name string) (uint16, []string, error) {
+
+	r.RLock()
+	svc, ok := r.byname[name]
+	r.RUnlock()
+
+	if !ok {
+		return 0, nil, fmt.Errorf("Name \"%q\" not found in the port registry", name)
+	}
+
+	return svc.port, svc.addr, nil
+}
+
+func (r *Registry) Alloc(name string, addr ...string) (uint16, error) {
+
+	r.Lock()
+	defer r.Unlock()
+
+	port, err := r.portAlloc()
+	if err != nil {
+		return 0, err
+	}
+
+	r.setSvc(port, name, addr...)
+
+	return port, nil
+}
+
+func (r *Registry) Forget(port uint16) {
+
+	r.Lock()
+	defer r.Unlock()
+
+	delete(r.byport, port)
+
+	if svc, ok := r.byport[port]; ok {
+		delete(r.byname, svc.name)
+	}
+
+	if port < r.port_next {
+		r.port_next = port
+	}
+}
+
+func (r *Registry) Fixed(port uint16, name string, addr ...string) error {
+
+	r.Lock()
+	defer r.Unlock()
+
+	_, name_taken := r.byname[name]
+	_, port_taken := r.byport[port]
+
+	if name_taken && port_taken {
+		return fmt.Errorf("Name %q and port %d are already registered", name, port)
+	}
+
+	if name_taken {
+		return fmt.Errorf("Name %q is already taken", name)
+	}
+
+	if port_taken {
+		return fmt.Errorf("Port %d is already taken", port)
+	}
+
+	r.setSvc(port, name, addr...)
+
+	return nil
+}
+
+type service struct {
+	port uint16
+	name string
+	addr []string
+}
+
+func (r *Registry) portAlloc() (uint16, error) {
+
+	for p, next := r.port_next, r.port_next <= r.port_max; next; p, next = p+1, p < r.port_max {
+
+		_, taken := r.byport[p]
+
+		if !taken {
+			return p, nil
+		}
+	}
+
+	return 0, fmt.Errorf("No ports available")
+}
+
+func (r *Registry) setSvc(port uint16, name string, addr ...string) {
+	svc := &service{port, name, addr}
+	r.byname[name] = svc
+	r.byport[port] = svc
+}
---
+++ internal/registry/registry_test.go
@@ -0,0 +1,205 @@
+/*
+	(c) 2015 Vlad Didenko. All Rights Reserved.
+
+	This file is part of Port ALLocator tool a.k.a. pall.
+
+	pall is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	pall is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with pall.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+package registry
+
+import (
+	"fmt"
+	"testing"
+)
+
+// An overflow concern relevant to the code and tests
+// is clarified at the StackOverflow question at
+// http://stackoverflow.com/questions/29880038
+
+// Test the creation of a registry with misc bounds
+// and flipped boundaries
+func TestNew(t *testing.T) {
+
+	boundsSet := [][2]uint16{
+		{0, 0}, {2, 2}, {0, 2}, {0, 65535},
+		{65534, 65535}, {65535, 65535},
+	}
+
+	for _, bounds := range boundsSet {
+		_, err := New(bounds[0], bounds[1])
+		if err != nil {
+			t.Error(err)
+		}
+	}
+
+	_, err := New(1, 0)
+	if err == nil {
+		t.Error("Registry creation should fail with min=1 and max=0")
+	}
+}
+
+// Test port allocation at the high end - catch potential overflow bugs
+func TestAddAllocHigh(t *testing.T) {
+
+	const (
+		max uint16 = ^uint16(0)
+		min uint16 = max - 2
+	)
+
+	reg, err := New(min, max)
+	if err != nil {
+		t.Error(err)
+	}
+
+	for i := min; i <= max; i++ {
+		p, err := reg.Alloc(fmt.Sprintf("svc%d", i), fmt.Sprintf("%d.%d.%d.%d", i, i, i, i))
+		if err != nil {
+			t.Errorf("Alloc registration # %d should not fail in [%d, %d] registry",
+				i, min, max)
+		}
+
+		if p != i {
+			t.Errorf("Allocated port %d should have been same as the iterator %d", p, i)
+		}
+
+		if i == max {
+			break
+		}
+	}
+}
+
+// Test generic use cases of fixing, allocating, querying,
+// and forgetting port assignments
+func TestRegistry(t *testing.T) {
+	const (
+		min uint16 = 0
+		max uint16 = 3
+	)
+
+	reg, err := New(min, max)
+	if err != nil {
+		t.Errorf("Failed to create a registry with min=%d and max=%d", min, max)
+	}
+
+	mocks := []struct {
+		port  uint16
+		name  string
+		addok bool
+		del   bool
+	}{
+		{port: 2, name: "fixed", addok: true, del: false},
+		{port: 0, name: "fixed", addok: true, del: false},
+		{port: 1, name: "alloc", addok: true, del: true},
+		{port: 3, name: "alloc", addok: true, del: false},
+		{port: 1, name: "fixed", addok: false, del: true},
+		{port: 4, name: "alloc", addok: false, del: false},
+		{port: 9, name: "fixed", addok: true, del: true},
+	}
+
+	mockMap := make(map[uint16]int)
+
+	for _, mock := range mocks {
+
+		name := fmt.Sprintf("%s %d", mock.name, mock.port)
+
+		if mock.name == "fixed" {
+
+			err = reg.Fixed(mock.port, name)
+			if (err == nil) != mock.addok {
+				t.Error(err)
+				continue
+			}
+
+		} else {
+
+			_, err = reg.Alloc(name)
+			if (err == nil) != mock.addok {
+				t.Error(err)
+				continue
+			}
+		}
+	}
+
+	for line, mock := range mocks {
+		if mock.del {
+			reg.Forget(mock.port)
+		} else {
+			if mock.addok {
+				mockMap[mock.port] = line
+			}
+		}
+	}
+
+	pmax := ^uint16(0)
+	for p, next := uint16(0), 0 < pmax; next; p, next = p+1, p < pmax {
+
+		midx, mOk := mockMap[p]
+
+		svc, sOk := reg.byport[p]
+
+		if sOk == mOk {
+			if mOk && svc.name != fmt.Sprintf("%s %d", mocks[midx].name, mocks[midx].port) {
+				t.Errorf("Service names do not match for port %d", p)
+				if err := matches(reg, mocks[midx].name, mocks[midx].port); err != nil {
+					t.Error(err)
+				}
+			}
+
+		} else if sOk {
+			t.Errorf("Unexpected service registered at port %d as %q", p, svc.name)
+
+		} else if mocks[midx].addok && !mocks[midx].del {
+			t.Errorf("Missing service expected at port %d as %q", p, mocks[midx].name)
+		}
+	}
+}
+
+func matches(r *Registry, name string, port uint16) error {
+	p, _, err := r.Query(name)
+	if err != nil {
+		return err
+	}
+	if p != port {
+		return fmt.Errorf("Querying %q returned %d instead of expected %d",
+			name, p, port)
+	}
+	return nil
+}
+
+// Benchmark of allocation. Hopefully no software will allocate all
+// 64K ports as this will get ugly (hash map lookups and mallocs)
+func BenchmarkAddAlloc(b *testing.B) {
+
+	const (
+		min uint16 = 0
+		max uint16 = ^uint16(0)
+	)
+
+	reg, err := New(min, max)
+	if err != nil {
+		b.Error(err)
+	}
+
+	for i := uint16(0); i < uint16(b.N); i++ {
+
+		_, err := reg.Alloc(fmt.Sprintf("service %d", i))
+
+		if err != nil {
+			b.Errorf("Alloc registration %d should not fail in [%d, %d] registry",
+				i, min, max)
+		}
+
+	}
+}
