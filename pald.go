8e0a983f320a6df651b64f490d7419e8238d9dbf
Vlad Didenko <git@didenko.com>
2015-05-11 00:30:49 -0500

    #7 Use github.com/takama/daemon to daemonise

--- pald.go
+++ pald.go
@@ -16,8 +16,70 @@
 
 package main
 
-import "github.com/didenko/pald/internal/server"
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/didenko/pald/internal/server"
+	"github.com/takama/daemon"
+)
+
+const (
+	daemonName = "pald"
+	daemonDesc = "Port Allocator Daemon"
+)
+
+var stdlog, errlog *log.Logger
+
+// Service has embedded daemon
+type Service struct {
+	daemon.Daemon
+}
+
+func (service *Service) Manage() (string, error) {
+	usage := fmt.Sprintf("Usage: %s install | remove | start | stop | status", daemonName)
+
+	// if received any kind of command, do it
+	if len(os.Args) > 1 {
+		command := os.Args[1]
+		switch command {
+		case "install":
+			return service.Install()
+		case "remove":
+			return service.Remove()
+		case "start":
+			return service.Start()
+		case "stop":
+			return service.Stop()
+		case "status":
+			return service.Status()
+		default:
+			return usage, nil
+		}
+	}
+
+	server.Run(8001, 49200, 49299)
+	// never happen, but need to complete code
+	return usage, nil
+}
+
+func init() {
+	stdlog = log.New(os.Stdout, "", log.Ldate|log.Ltime)
+	errlog = log.New(os.Stderr, "", log.Ldate|log.Ltime)
+}
 
 func main() {
-	server.Run(8001, 49200, 49299)
+	srv, err := daemon.New(daemonName, daemonDesc)
+	if err != nil {
+		errlog.Println("Error: ", err)
+		os.Exit(1)
+	}
+	service := &Service{srv}
+	status, err := service.Manage()
+	if err != nil {
+		errlog.Println(status, "\nError: ", err)
+		os.Exit(1)
+	}
+	fmt.Println(status)
 }
